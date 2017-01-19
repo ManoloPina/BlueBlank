c115697e67c464a0742e0256007f8b1d5c4ef43c
Manolo Pina <contato@manolopina.com.br>
2017-01-19 12:25:35 -0200

    + update

--- routes/transfer.js
+++ routes/transfer.js
@@ -2,11 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
-const app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-router.use('/',  (req, res) => {
-  res.send('transfer');
+router.all('/', (req, res) => {
+  res.send(req.body);
 })
 
 module.exports = router;
